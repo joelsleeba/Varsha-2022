5303eb76b91b69d663ac6b7dc6780129adc2aefc
joelsleeba <joelsleeba1@gmail.com>
2022-08-24 14:33:39 +0530

    changed edit branch name

--- pull.sh
+++ pull.sh
@@ -1,6 +1,6 @@
 git checkout source
 git pull source main
-git checkout pre-edit
+git checkout edit
 git rm -rf "*.log" "*.aux" "*.pdf" "*.out" "*.syn*" "*.latexmain"
 echo "ready for merge from source"
 echo "run: git merge source"
--- push.sh
+++ push.sh
@@ -1,16 +1,17 @@
 echo -n "Enter commit message:"
 read MESSAGE
 
+./pdfgen.sh
 git add .
 git commit -m "$MESSAGE"
-git push -u my-repo pre-edit
+git push -u my-repo edit
 
 git checkout main
-git merge pre-edit
+git merge edit
 git push -u my-repo main
 
 git checkout source
 git pull source main
 git push -u my-repo source
 
-git checkout pre-edit
+git checkout edit
