6e8b410281434c1df11a8627efa77e8ea58816dd
joelsleeba <joelsleeba1@gmail.com>
2022-08-24 15:03:50 +0530

    remote name changed

--- push.sh
+++ push.sh
@@ -4,14 +4,14 @@ read MESSAGE
 ./pdfgen.sh
 git add .
 git commit -m "$MESSAGE"
-git push -u my-repo edit
+git push -u myrepo edit
 
 git checkout main
 git merge edit
-git push -u my-repo main
+git push -u myrepo main
 
 git checkout source
 git pull source main
-git push -u my-repo source
+git push -u myrepo source
 
 git checkout edit
